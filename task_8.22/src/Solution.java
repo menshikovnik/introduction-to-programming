d74d32d807195aebe4e3089cdb28b5d90cfb1a78
Nikolai Menshikov <kolya.menshikov.03@inbox.ru>
2023-03-16 20:30:38 +0300

    Add files via upload

---
+++ task_8.22/src/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+
+//    public static void isFriendly(int[][] myArray, int  i, int j){
+//        int count = 0;
+//        i = 0;
+//        j = 0;
+////        for ( int i = 0;i < myArray.length;i++){
+////            for (int j = 0;j < myArray[0].length;j++){
+//                if (i < myArray.length - 1 && myArray[i][j] == myArray[i + 1][j]){ // проверяет элементы снизу
+//                    count ++;
+////                    i += 1;
+////                    isFriendly(myArray, i, j);
+//                }
+//                if (j == myArray[0].length - 1)break;
+//                if (myArray[i][j] == myArray[i][j + 1]){ // проверяет элементы справа
+//                    count ++;
+//                }
+//            }
+        }
+//        System.out.println("Количество дружественных элементов " + count);
+
